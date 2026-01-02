3f5604155b82d5017ea28a53a91ae245612ec8b9
Lucas Francisco <lucasbarroso318@gmail.com>
2026-01-01 23:06:06 -0300

    fix: correcting light rays and add event card details

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -27,7 +27,21 @@ export default function EventCard({
 				height={300}
 				className="poster"
 			/>
+			<div className="flex gap-2">
+				<Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+				<p>{location}</p>
+			</div>
 			<p className="title">{title}</p>
+			<div className="datetime">
+				<div>
+					<Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+					<p>{date}</p>
+				</div>
+				<div>
+					<Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+					<p>{time}</p>
+				</div>
+			</div>
 		</Link>
 	)
 }
